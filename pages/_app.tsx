4603195505c45389b9872dd17120ec8108e89fb8
Diego <lucca86@gmail.com>
2022-10-01 19:00:05 -0300

    almacen crudo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app'
+import { Box, ChakraProvider, Container, Divider, Heading, Image, Text, VStack } from '@chakra-ui/react';
+import theme from '../theme';
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box padding={4}>
+        <Container
+          borderRadius='sm'
+          backgroundColor='white'
+          boxShadow='md'
+          maxWidth='container.xl'
+          padding={4}
+        >
+          <VStack marginBottom={6} >
+            <Image borderRadius={9999} src='//placehold.it/128x128'></Image>
+            <Heading>Almacen</Heading>
+            <Text>Almacen by Goncy</Text>
+          </VStack>
+          <Divider marginY={6} />
+          <Component {...pageProps} />
+        </Container>
+      </Box>
+    </ChakraProvider>
+
+  )
 }
 
-export default MyApp
+export default App
